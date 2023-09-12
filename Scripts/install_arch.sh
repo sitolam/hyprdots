5cc0164cb4c2602d2284fe3cd97220f36046d2cf
sitolam <79202140+Sitolam@users.noreply.github.com>
2023-09-12 21:10:10 +0200

    Updated install_arch.sh

--- Scripts/install_arch.sh
+++ Scripts/install_arch.sh
@@ -15,4 +15,19 @@ export LC_ADDRESS="nl_BE"
 export LC_MEASUREMENT="nl_BE"
 export LC_NAME="nl_BE"
 EOF
-```
+
+cat > /etc/locale.conf << EOF
+LANG=en_US.UTF-8
+#export LANGUAGE="en_US:en"
+LC_MESSAGES="en_US.UTF-8"
+LC_CTYPE="nl_BE@euro"
+LC_COLLATE="nl_BE@euro"
+LC_TIME="nl_BE"
+LC_NUMERIC="nl_BE"
+LC_MONETARY="nl_BE@euro"
+LC_PAPER="nl_BE"
+LC_TELEPHONE="nl_BE"
+LC_ADDRESS="nl_BE"
+LC_MEASUREMENT="nl_BE"
+LC_NAME="nl_BE"
+EOF
