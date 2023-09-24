1ab66484478414acc12554600986ce372740a2c5
sitolam <79202140+Sitolam@users.noreply.github.com>
2023-09-24 13:51:56 +0200

    Started the install_arch script added online check

--- Scripts/install_arch.sh
+++ Scripts/install_arch.sh
@@ -1,33 +1,19 @@
 #!/bin/bash
-cat << EOF >> /etc/profile
-#locale settings
-export LANG=en_US.UTF-8
-#export LANGUAGE="en_US:en"
-export LC_MESSAGES="en_US.UTF-8"
-export LC_CTYPE="nl_BE@euro"
-export LC_COLLATE="nl_BE@euro"
-export LC_TIME="nl_BE"
-export LC_NUMERIC="nl_BE"
-export LC_MONETARY="nl_BE@euro"
-export LC_PAPER="nl_BE"
-export LC_TELEPHONE="nl_BE"
-export LC_ADDRESS="nl_BE"
-export LC_MEASUREMENT="nl_BE"
-export LC_NAME="nl_BE"
-EOF
+#|---/ /+----------------------------------------+---/ /|#
+#|--/ /-| Script to install arch                  |--/ /-|#
+#|-/ /--| Sitolam                                 |-/ /--|#
+#|/ /---+----------------------------------------+/ /---|#
 
-cat > /etc/locale.conf << EOF
-LANG=en_US.UTF-8
-#export LANGUAGE="en_US:en"
-LC_MESSAGES="en_US.UTF-8"
-LC_CTYPE="nl_BE@euro"
-LC_COLLATE="nl_BE@euro"
-LC_TIME="nl_BE"
-LC_NUMERIC="nl_BE"
-LC_MONETARY="nl_BE@euro"
-LC_PAPER="nl_BE"
-LC_TELEPHONE="nl_BE"
-LC_ADDRESS="nl_BE"
-LC_MEASUREMENT="nl_BE"
-LC_NAME="nl_BE"
-EOF
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+clear
+echo "${bold}Welcome to the installation part of arch${normal}"
+
+
+wget -q --tries=10 --timeout=20 --spider http://google.com
+if [[ $? -eq 0 ]]; then
+    echo "Online"
+else
+    echo "Offline"
+fi
