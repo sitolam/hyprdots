9d660eaa2616baea17e5120a80db581ca1454a84
sitolam <79202140+Sitolam@users.noreply.github.com>
2023-09-26 22:03:39 +0200

    Added keyboard layout selector

--- Scripts/install_arch.sh
+++ Scripts/install_arch.sh
@@ -9,8 +9,28 @@ normal=$(tput sgr0)
 
 clear
 echo "${bold}Welcome to the installation part of arch${normal}"
+sleep 1
+clear
 
-
+echo "${bold}Do you want to change your keyboard layout?${normal}"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) echo "Do you want to search your keyboard layout by giving your language, country or layout name?"
+              select yn in "Yes" "No"; do
+                  case $yn in
+                      Yes ) read -p "Search term: " search_term
+                            localectl list-keymaps | grep -i $search_term; break;;
+                      No ) break;;
+                  esac
+              done
+              read -p "Layout: " key_layout
+              loadkeys $key_layout
+              ; break;;
+        No ) break;;
+    esac
+done
+sleep 1
+clear
 wget -q --tries=10 --timeout=20 --spider http://google.com
 if [[ $? -eq 0 ]]; then
     echo "${bold}Online${normal}"
