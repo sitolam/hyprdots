44e882573ae97955820eeff3f5e9ebad3ac3bb84
sitolam <79202140+Sitolam@users.noreply.github.com>
2023-09-26 21:54:40 +0200

    Connect to wifi part done

--- Scripts/install_arch.sh
+++ Scripts/install_arch.sh
@@ -13,7 +13,34 @@ echo "${bold}Welcome to the installation part of arch${normal}"
 
 wget -q --tries=10 --timeout=20 --spider http://google.com
 if [[ $? -eq 0 ]]; then
-    echo "Online"
+    echo "${bold}Online${normal}"
 else
-    echo "Offline"
+    echo "${bold}Offline${normal}"
+    echo ""
+    while [[ $? -eq 0 ]]
+    do
+        iwctl device list
+        echo "What is your network interface?"
+        read -p "Interface: " interface
+        clear
+
+        iwctl station $interface scan
+        iwctl station $interface get-networks
+        echo "To which network you want to connect? "
+        read -p "Network: " network
+        clear
+
+        echo "What is the password for the network? "
+        read -p "Password: " password
+        clear
+
+        echo "${bold}Connecting ......${normal}"
+        iwctl --passphrase=$password station $interface connect $network
+        sleep 1
+        clear
+
+        echo "${bold}Waiting to connect .....${normal}"
+        sleep 10
+        clear
+    done
 fi
