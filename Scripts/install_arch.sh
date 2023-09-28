e5f028d57f7428b6ce97116cacfabd9d36e5a878
sitolam <79202140+Sitolam@users.noreply.github.com>
2023-09-28 20:19:41 +0200

    Added setting the right locales

--- Scripts/install_arch.sh
+++ Scripts/install_arch.sh
@@ -219,6 +219,40 @@ locale-gen
 sleep 1
 clear
 
+# Set the right locales
+echo "${bold}Setting the right locales ........ ${normal}"
+cat << EOF >> /etc/profile
+#locale settings
+export LANG=en_US.UTF-8
+#export LANGUAGE="en_US:en"
+export LC_MESSAGES="en_US.UTF-8"
+export LC_CTYPE="nl_BE@euro"
+export LC_COLLATE="nl_BE@euro"
+export LC_TIME="nl_BE"
+export LC_NUMERIC="nl_BE"
+export LC_MONETARY="nl_BE@euro"
+export LC_PAPER="nl_BE"
+export LC_TELEPHONE="nl_BE"
+export LC_ADDRESS="nl_BE"
+export LC_MEASUREMENT="nl_BE"
+export LC_NAME="nl_BE"
+EOF
+cat > /etc/locale.conf << EOF
+#locale settings
+LANG=en_US.UTF-8
+#export LANGUAGE="en_US:en"
+LC_MESSAGES="en_US.UTF-8"
+LC_CTYPE="nl_BE@euro"
+LC_COLLATE="nl_BE@euro"
+LC_TIME="nl_BE"
+LC_NUMERIC="nl_BE"
+LC_MONETARY="nl_BE@euro"
+LC_PAPER="nl_BE"
+LC_TELEPHONE="nl_BE"
+LC_ADDRESS="nl_BE"
+LC_MEASUREMENT="nl_BE"
+LC_NAME="nl_BE"
+EOF
 
 exit # to leave the chroot
 EOF
