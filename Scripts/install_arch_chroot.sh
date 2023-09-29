64f6c65aa7857205fbb20dfb22698ea2504e5db5
sitolam <79202140+Sitolam@users.noreply.github.com>
2023-09-29 20:38:34 +0200

    Added the install the theme part

--- Scripts/install_arch_chroot.sh
+++ Scripts/install_arch_chroot.sh
@@ -167,6 +167,42 @@ clear
 
 
 
-# Root password
+# Install the theme
+echo "${bold}Installing the theme ........${normal}"
+sleep 0.5
 
+echo "Changing to the user account ........"
+su $username
+sleep 0.5
+
+echo "Making the directory ........"
+mkdir ~/hyprdots
+sleep 0.5
+
+echo "Changing the directory ........"
+cd ~/hyprdots
+sleep 0.5
+
+echo "Cloning the theme ........"
+git clone https://github.com/Sitolam/hyprdots
+cd hyprdots/Scripts
+sleep 1
+
+clear
+
+echo "${bold}Are you ready?!${normal}"
+sleep 1
+clear
+
+echo "${bold}Installing the theme ........ ${normal}"
+sleep 0.5
+./install.sh -drs custom_apps.lst
+sleep 1
+clear
+
+echo "Installing the flatpaks ........"
+./.extra/install_fpk.sh
+
+
+exit # leave the user account
 exit # to leave the chroot
