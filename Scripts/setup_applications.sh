0438a7a6b0a7d1e7aa5b73a329fcfb9c388021b6
sitolam <79202140+Sitolam@users.noreply.github.com>
2023-10-29 20:19:28 +0100

    Added wayvnc

--- Scripts/setup_applications.sh
+++ Scripts/setup_applications.sh
@@ -9,4 +9,31 @@ bold=$(tput bold)
 normal=$(tput sgr0)\
 
 
+# Wayvnc
+mkdir -p ~/Downloads/github/wayvnc/
+cd ~/Downloads/github/wayvnc/
+sudo pacman -S base-devel libglvnd libxkbcommon pixman gnutls jansson
+
+git clone https://github.com/any1/wayvnc.git
+git clone https://github.com/any1/neatvnc.git
+git clone https://github.com/any1/aml.git
+
+mkdir wayvnc/subprojects
+cd wayvnc/subprojects
+ln -s ../../neatvnc .
+ln -s ../../aml .
+cd -
+
+mkdir neatvnc/subprojects
+cd neatvnc/subprojects
+ln -s ../../aml .
+cd -
+#TODO: automate patch https://github.com/any1/wayvnc/issues/248
+
+
+cd wayvnc
+
+meson build
+ninja -C build
+
 exit
