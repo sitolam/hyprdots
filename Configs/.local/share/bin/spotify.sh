b62b0fbc73636dd44891a7c16e24d0f58d03a16a
sitolam <otis.lammertyn@gmail.com>
2024-07-13 20:16:21 +0200

    feat: Improve Spotify launch script with switching to specific workspace

--- Configs/.local/share/bin/spotify.sh
+++ Configs/.local/share/bin/spotify.sh
@@ -1,20 +1,36 @@
 #!/usr/bin/env sh
 
+
 # set variables
 
 scrDir=`dirname "$(realpath "$0")"`
 source $scrDir/globalcontrol.sh
 
+if [ $# -eq 0 ] ; then
+    echo "usage: ./spotify.sh <spotify-title>"
+    exit 1
+else
+    spotifyTitle="$1"
+fi
+
 
 hyprctl dispatch focusworkspaceoncurrentmonitor 10
 
-kitty --title "spotify cava" sh -c "cava" &
-sleep 0.3
-hyprctl --batch "dispatch focuswindow cava ; dispatch togglefloating ; dispatch resizeactive exact 50% 55% ; dispatch centerwindow 1 ; dispatch moveactive 70% 60%" 
 
-spotify &
-sleep 1
-hyprctl --batch "dispatch focuswindow initialTitle:^(Spotify Premium)$ ; dispatch resizeactive exact 50% 55% ; dispatch centerwindow 1 ; dispatch moveactive -70% -60%" 
+if [ -z "$(hyprctl clients | grep "$spotifyTitle" | grep -v "grep" | grep -v "./spotify.sh")" ]; then
+    kitty --title "spotify cava" sh -c "cava" &
+    sleep 0.3
+    hyprctl --batch "dispatch focuswindow cava ; dispatch togglefloating"
+    hyprctl --batch "dispatch focuswindow cava ; dispatch resizeactive exact 50% 55%"
+    hyprctl --batch "dispatch focuswindow cava ; dispatch centerwindow 1"
+    hyprctl --batch "dispatch focuswindow cava ; dispatch moveactive 70% 60%"
 
-sleep 3
-playerctl play-pause
+    spotify &
+    sleep 1
+    hyprctl --batch "dispatch focuswindow initialTitle:^($spotifyTitle)$ ; dispatch resizeactive exact 50% 55%"
+    hyprctl --batch "dispatch focuswindow initialTitle:^($spotifyTitle)$ ; dispatch centerwindow 1"
+    hyprctl --batch "dispatch focuswindow initialTitle:^($spotifyTitle)$ ; dispatch moveactive -70% -60%"
+
+    sleep 3
+    playerctl play-pause
+fi
