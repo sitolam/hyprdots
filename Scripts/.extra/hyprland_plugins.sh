77188edeb44171340f25dd94065efb3338411f2b
sitolam <otis.lammertyn@gmail.com>
2024-07-14 15:21:37 +0200

    feat: improved hyprland_plugins installer to work with a lst file

--- Scripts/.extra/hyprland_plugins.sh
+++ Scripts/.extra/hyprland_plugins.sh
@@ -4,10 +4,33 @@
 #|-/ /--| Prasanth Rangan                   |-/ /--|#
 #|/ /---+-----------------------------------+/ /---|#
 
+# Set varables
+scrDir=$(dirname "$(realpath "$0")")
+
+
+source "${scrDir}/../global_fn.sh"
+if [ $? -ne 0 ]; then
+    echo "Error: unable to source global_fn.sh..."
+    exit 1
+fi
+
+CfgLst="${1:-"${scrDir}/hyprland_plugins.lst"}"
+
+
+# Updating
 hyprpm update # Update hyprpm and install hyprland headers
 
-hyprpm list | grep -A 1 "Plugin Hyprspace" | grep true
-if [ $? -eq 0 ]; then
-    hyprpm add https://github.com/ReshetnikovPavel/Hyprspace # Install the hyprspace plugin
-    hyprpm enable Hyprspace # Enable the hyprspace plugin
-fi
+# Installing plugins
+cat "${CfgLst}" | while read lst; do
+
+    name=$(echo "${lst}" | awk -F '|' '{print $1}')
+    repo=$(echo "${lst}" | awk -F '|' '{print $2}')
+
+    
+    if [ -z "$(hyprpm list | grep -A 1 "Plugin ${name}" | grep true)" ]; then
+        hyprpm add "$repo" # Install the hyprspace plugin
+        hyprpm enable "$name" # Enable the hyprspace plugin
+    fi
+
+done
+
