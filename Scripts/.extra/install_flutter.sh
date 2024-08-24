9688be5817e37e753b1a946e5a6a2e0505e7a7f3
sitolam <otis.lammertyn@gmail.com>
2024-08-24 10:19:05 +0200

    feat: add install_flutter script

---
+++ Scripts/.extra/install_flutter.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+#|---/ /+-----------------------------------+---/ /|#
+#|--/ /-| Script to install flatpaks (user) |--/ /-|#
+#|-/ /--| Prasanth Rangan                   |-/ /--|#
+#|/ /---+-----------------------------------+/ /---|#
+
+baseDir=$(dirname "$(realpath "$0")")
+scrDir=$(dirname "$(dirname "$(realpath "$0")")")
+
+source "${scrDir}/global_fn.sh"
+if [ $? -ne 0 ]; then
+    echo "Error: unable to source global_fn.sh..."
+    exit 1
+fi
+chk_list "aurhlpr" "${aurList[@]}"
+
+use_default="--noconfirm --needed"
+aurhPkg=(flutter-bin android-sdk android-sdk-build-tools android-sdk-cmdline-tools-latest android-platform android-sdk-platform-tools)
+
+if [[ ${#aurhPkg[@]} -gt 0 ]]; then
+    "${aurhlpr}" ${use_default} -Sy "${aurhPkg[@]}"
+fi
+
+sudo cp -R /opt/android-sdk ~
+
+cd ~
+sudo chown -R $USER:$(groups ${USER} | awk '{print $1}') android-sdk
+
+echo 'export ANDROID_HOME=$HOME/android-sdk' >> $HOME/.zshrc
+echo 'export PATH=$PATH:$ANDROID_HOME/platform-tools' >> $HOME/.zshrc
+echo 'export JAVA_HOME=/usr/lib/jvm/java-17-openjdk' >> $HOME/.zshrc
+
+yes | flutter doctor --android-licenses
